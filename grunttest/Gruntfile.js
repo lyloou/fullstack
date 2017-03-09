aed4a6272aceb3037218cd39be41988a46e75b82
Lou <lyloou6@gmail.com>
2017-03-09 18:35:44 +0800

    fix grunt bug

--- grunttest/Gruntfile.js
+++ grunttest/Gruntfile.js
@@ -1,20 +1,54 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
-    	pkg: grunt.file.readJSON('package.json'),
-        uglify: {
-			 options: {
-				 banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %>*/\n'
-			 },
-			 build: {
-				 src: 'src/<%= pkg.name %>.js',
-				 dest: 'build/<%= pkg.name %>.min.js'
-			 }
-		 }
+		pkg: grunt.file.readJSON('package.json'),
+		uglify: {
+			options: {
+				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %>*/\n'
+			},
+			build: {
+				src: 'src/<%= pkg.name %>.js',
+				dest: 'build/<%= pkg.name %>.min.js'
+			}
+		},
+		concat: {
+			options: {
+				separator: ";"
+			},
+			dist: {
+				src: ['src/**/*.js'],
+				dest: 'dist/<%= pkg.name %>.js'
+			}
+		},
+		qunit: {
+			files: ['test/**/*.html']
+		},
+		jshint: {
+			files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+			options: {
+				globals: {
+					jQuery: true,
+					console: true,
+					module: true
+				}
+			}
+		},
+		watch: {
+			files: ['<%= jshint.files %>'],
+			tasks: ['jshint', 'qunit']
+		}
+
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.registerTask('default', ['uglify']);
-	grunt.registerTask('log', 'log some stuff', function () {
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-qunit');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.registerTask('test', ['jshint', 'qunit']);
+	grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
+
+	grunt.registerTask('log', 'log some stuff', function() {
 		grunt.log.write('Logging some stuff ...').error();
 	});
-}
+};
